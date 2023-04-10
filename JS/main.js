8196d8f4a18d2e1625d1b38a875ac9ed821e123b
EdoardaDepine <edoarda.depine@gmail.com>
2023-04-10 15:21:50 -0300

    tornando a função dinâmica

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,12 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
   try {
-    const consultaCep = await fetch("https://viacep.com.br/ws/010010000/json/");
+    const consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     const consultaCepConvertida = await consultaCep.json();
     if (consultaCepConvertida.erro) {
       throw Error("CEP não existente!");
     } else {
       console.log(consultaCepConvertida);
+      return consultaCepConvertida;
     }
   } catch (erro) {
     console.log(erro);
